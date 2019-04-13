b4ab0c79a0aa399fd6e8076f76c41699c59ddf77
EliteFan <fan552426811@outlook.com>
2019-04-13 11:26:24 +0800

    second

--- js/main.js
+++ js/main.js
@@ -1,28 +1,63 @@
 var result = `
 /*
-动画开始啦，
-哈哈
+ * 3 2 1 开始！
+ * 为了清楚表达，让我们显示代码的同时高亮代码。
 */
 
-*{
-  transition: all 1s;
+* {
+    margin: 0;
+    padding: 0;
+    transition: all 1s;
 }
-body{
-  background-color: #ddd;
+
+body {
+    background-color: #ddd;
 }
-#sourceCode{
-    padding:16px;
+
+#sourceCode {
+    border: 1px solid red;
+    padding: 16px;
+    transform:rotate(360deg);
 }
+
 /*加点样式*/
 `
 
 var n = 0
 var id = setInterval(function () {
+  n++
+  sourceStyle.innerHTML += result.slice(n - 1, n)
+  // sourceCode.innerHTML += result.slice(n - 1, n)
+  sourceCode.innerHTML = Prism.highlight(sourceStyle.innerHTML, Prism.languages.css);
+  if (n >= result.length) {
+    window.clearInterval(id)
+    creatPaper()
+    fn3()
+  }
+}, 100)
+
+function creatPaper() {
+  var paper = document.createElement('div')
+  paper.id = 'paper'
+  document.body.appendChild(paper)
+}
+
+function fn3() {
+  var result = `
+  #paper{
+    width: 200px;
+    height: 200px;
+    border: 1px solid red;
+  }
+  `
+  var n = 0
+  var id = setInterval(function () {
     n++
-    sourceStyle.innerHTML = result.slice(0, n)
-    sourceCode.innerHTML = result.slice(0, n)
+    sourceStyle.innerHTML += result.slice(n - 1, n)
+    // sourceCode.innerHTML += result.slice(n - 1, n)
     sourceCode.innerHTML = Prism.highlight(sourceStyle.innerHTML, Prism.languages.css);
     if (n >= result.length) {
-        window.clearInterval(id)
+      window.clearInterval(id)
     }
-}, 100)
+  }, 100)
+}
